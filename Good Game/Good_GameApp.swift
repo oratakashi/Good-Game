10ddff577336013cbfbb6401c041c73609b3efbc
Oratakashi Nhamako <davidcreazy@gmail.com>
2021-07-13 21:00:58 +0700

    Initial Commit

---
+++ Good Game/Good_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_GameApp.swift
+//  Good Game
+//
+//  Created by oratakashi on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Good_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
